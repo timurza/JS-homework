163a663131dfe358cb7f4848d76fb7e4a1149f27
timurza <timur88info@gmail.com>
2022-01-11 15:34:07 +0300

    feat: add folder hw4

--- hw3/script.js
+++ hw3/script.js
@@ -1,59 +1,111 @@
 //Functions
-/*
 //Task 0 (1 method )
-function sayHello(name){
-    if(name === 'oleg'){
+function sayHello(name) {
+    if (name === 'oleg') {
         return (`Hi  ${name}`);
-    }
-    else{
-        return (`Hello  ${name}`); 
+    } else {
+        return (`Hello  ${name}`);
     }
 }
 console.log(sayHello('oleg'));
 
 //(2 method )
 let name = prompt('what is your name?')
-if(name === 'oleg'){
-    alert (`Hi  ${name}`);
-}
-else{
-    alert (`Hello  ${name}`); 
+if (name === 'oleg') {
+    alert(`Hi  ${name}`);
+} else {
+    alert(`Hello  ${name}`);
 }
 
 //Task 1
 
-function min(a,b){
-    if(a > b){
+function min(a, b) {
+    if (a > b) {
         return b;
     }
     return a;
 }
-console.log(min(-1,37));
+console.log(min(-1, 37));
 
 
 //Task 2
 
-function pow(x,n){
-    let result = x**n;
-    return result;   
+function pow(x, n) {
+    let result = x ** n;
+    return result;
 }
-console.log(pow(3,3));
+console.log(pow(3, 3));
 
 
 //Task 3
 
-function isEven(n){
-    if(n%2 === 0){
+function isEven(n) {
+    if (n % 2 === 0) {
         return true;
     }
     return false;
 }
 console.log(isEven(19));
-*/
+
 
 //Task 4
 
-function deleteChars(str){
-    return str.substr(1, str.length-2 )
+function deleteChars(str) {
+    return str.substr(1, str.length - 2)
 }
 console.log(deleteChars('hello'));
+
+
+//Task 6
+function convertType(value, toType) {
+    if (toType === 'boolean') {
+        return Boolean(value);
+    } else if (toType === 'string') {
+        return String(value);
+    } else if (toType === 'number') {
+        return Number(value);
+
+    }
+}
+console.log(convertType('my string', 'boolean'));*/
+
+let convertType = (value, toType) => {
+    if (toType === 'boolean') {
+        return Boolean(value);
+    } else if (toType === 'string') {
+        return String(value);
+    } else if (toType === 'number') {
+        return Number(value);
+
+    }
+}
+console.log(convertType('my string', 'boolean'));
+
+//function Expression
+
+let convertType = function (value, toType) {
+    if (toType === 'boolean') {
+        return Boolean(value);
+    } else if (toType === 'string') {
+        return String(value);
+    } else if (toType === 'number') {
+        return Number(value);
+    }
+}
+console.log(convertType('my string', 'boolean'));
+
+
+
+let convertType = (value, toType) => {
+    if (toType === 'boolean') {
+        return Boolean(value);
+    } else if (toType === 'string') {
+        return String(value);
+    } else if (toType === 'number') {
+        return Number(value);
+
+    }
+}
+console.log(convertType('my string', 'boolean'));
+
+
---
+++ hw4/script.js
@@ -0,0 +1,30 @@
+ //Task 1
+ const p = document.createElement("p");
+ p.className = 'text';
+ p.innerHTML = "Это всего лишь текст";
+ document.body.appendChild(p);
+ const div = document.createElement("div");
+ div.className = 'alert';
+ div.innerHTML = 'mistake';
+ document.body.appendChild(div);
+
+
+ //Task2
+ let arr = ["Viktor", "Tatiana", "Eduard", "Michael", "Denis", "Peter", "Ann", "Dmitry",
+     "Sergey", "Ivan", "Alan"
+ ];
+ let ol = document.createElement("ol");
+ for (let i = 0; i < arr.length; i++) {
+     let li = document.createElement("li");
+     li.innerText = arr[i];
+     if (i % 2 === 0) {
+         li.className = "even";
+         li.style.backgroundColor = "green";
+         ol.append(li);
+     } else {
+         li.className = "odd";
+         li.style.backgroundColor = "blue";
+         ol.append(li);
+     };
+ };
+ document.body.append(ol);
